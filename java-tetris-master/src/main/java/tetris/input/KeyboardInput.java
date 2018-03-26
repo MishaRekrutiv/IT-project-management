450ecb105131a2ab4bea6a8ee32b37838d3c0336
MishaRekrutiv <rekrutiv96@gmail.com>
2018-03-26 10:58:35 +0300

    Add my new project Tetris

---
+++ java-tetris-master/src/main/java/tetris/input/KeyboardInput.java
@@ -0,0 +1,11 @@
+package tetris.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class KeyboardInput implements KeyListener {
+
+    
+}
---
+++ java-tetris-master/src/test/java/tetris/model/BoardTest.java
@@ -0,0 +1,12 @@
+package tetris.model;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+
+
+}
